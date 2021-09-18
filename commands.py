49c40bfa2e2680bbeb705a21fa33e7947c17deb9
Asqaraliyev Faxriyor <asqaraliyev07@gmail.com>
2021-09-18 21:56:18 +0500

    added commands.py

---
+++ commands.py
@@ -0,0 +1,9 @@
+import click
+from falsk.cli import with_appcontext
+
+from database models import db, Actors, Movies
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+	db.create_tables()
