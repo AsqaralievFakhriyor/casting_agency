bd950f8fdebcb4127851c24850d13bf274e748fd
Asqaraliyev Faxriyor <asqaraliyev07@gmail.com>
2021-09-19 02:09:32 +0500

    bug-fixed

--- database/models.py
+++ database/models.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 
 # setup function 
 def setup_db(app):
-	app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:0909@localhost:5432/capstone'
+	app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://ohimydbamxhozq:4874b53b0e2a0403128a5f871f3007ea9e90c04c5f0c7590b24ae6b60ef1cfb1@ec2-63-32-7-190.eu-west-1.compute.amazonaws.com:5432/d59sliljil0g1o'
 	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 	db.app = app
 	# grabing configurations from config.py
