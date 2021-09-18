4152cbc0fecfcdd6cdfb70bd86708d64541d9909
Asqaraliyev Faxriyor <asqaraliyev07@gmail.com>
2021-09-19 01:59:40 +0500

    bug-fixed

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ export FLASK_APP=app
 export ENV='development'
 export FLASK_DEBUG=True
 
-# export DATABASE_URL='postgresql://postgres:0909@localhost:5432/capstone' # this only for developing
+export DATABASE_URL='postgresql://postgres:0909@localhost:5432/capstone' # this only for developing
