97712e1ef7efc595485d4c8ab1bdc31c22a4f86f
AsqaraliyevFakhriyor <84299503+AsqaraliyevFakhriyor@users.noreply.github.com>
2022-06-06 21:09:17 +0500

    removed some credentials

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ export FLASK_ENV='development'
 export FLASK_DEBUG=true
 
 """ DATABSE ENV """
-export DATABASE_URL='postgres://ohimydbamxhozq:4874b53b0e2a0403128a5f871f3007ea9e90c04c5f0c7590b24ae6b60ef1cfb1@ec2-63-32-7-190.eu-west-1.compute.amazonaws.com:5432/d59sliljil0g1o' # this only for developing
+export DATABASE_URL='' # this only for developing
 export DATABASE_TRACK_MODIFICATIONS=true
 
 """AUTH ENV"""
